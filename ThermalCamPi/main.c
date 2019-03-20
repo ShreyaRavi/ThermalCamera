bca04ec197adb5a5f00d87d65e660c48b09beb77
Alexa Ramachandran <arama@stanford.edu>
2019-03-20 14:20:35 -0700

    Updated file structure

--- ThermalCamPi/main.c
+++ ThermalCamPi/main.c
@@ -74,6 +74,56 @@ void test_data_get(void) {
     //Once params are extracted, we can release eeMLX90640 array
 }
 
+void test_data_transmit(void) {
+    thermal_init(20.0f, 40.0f, GRADIENT_IRON);
+    MLX90640_I2CFreqSet(1000);
+    uart_init();
+    MLX90640_I2CInit();
+
+
+    timer_delay_us(80000);
+
+    int status;
+    //Get device parameters - We only have to do this once
+    uint16_t eeMLX90640[832];
+    status = MLX90640_DumpEE(MLX90640_address, eeMLX90640);
+
+    if (status != 0) {
+        printf("Failed to load system parameters");
+    }
+
+    status = MLX90640_ExtractParameters(eeMLX90640, &mlx90640);
+    if (status != 0) {
+        printf("Parameter extraction failed, status: %d\n", status);
+    }
+
+    MLX90640_SetRefreshRate(MLX90640_address, 0x06); // 32 Hz -- 16 fps (Highest I can go i think) 
+
+    while (1) {
+        for (char x = 0; x < 2; x++) //Read both subpages
+        {
+            uint16_t mlx90640Frame[834];
+            int status = MLX90640_GetFrameData(MLX90640_address, mlx90640Frame);
+            if (status < 0)
+            {
+                printf("GetFrame Error: %d\n", status);
+            }
+
+            float vdd = MLX90640_GetVdd(mlx90640Frame, &mlx90640);
+
+            float Ta = MLX90640_GetTa(mlx90640Frame, &mlx90640);
+
+            float tr = Ta - TA_SHIFT; //Reflected temperature based on the sensor ambient temperature
+            float emissivity = 0.95;
+
+            MLX90640_CalculateTo(mlx90640Frame, &mlx90640, emissivity, tr, mlx90640To);
+            transmit_thermal_img(mlx90640To);
+        }
+    }
+    //Once params are extracted, we can release eeMLX90640 array
+}
+
+
 void test_thermal_color(void) {
     thermal_init(0.0, 80.0, GRADIENT_IRON);
 
